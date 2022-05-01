6c8b12402a0f1b0fee111810505aecf7e1c90bf5
Ishan Lal <ishanlal@gmail.com>
2022-05-01 22:13:22 +0800

    added endpoint tests

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ const resize = async (
   reqHeight: number
 ) => {
   const cwd = path.join(__dirname);
-  const inputPath = path.join(cwd, '..', 'images', inFileName);
-  const outputPath = path.join(cwd, '..', 'thumbnails', outFileName);
+  const inputPath = path.join(cwd, '../..', 'images', inFileName);
+  const outputPath = path.join(cwd, '../..', 'thumbnails', outFileName);
   await sharp(inputPath)
     .resize(reqWidth, reqHeight)
     .toFile(outputPath)
@@ -26,12 +26,14 @@ const resize = async (
 app.get('/convert', sharpLogger, async (req, res, next) => {
   const cwd = path.join(__dirname);
   const makeDir = async () => {
-    await promises.mkdir(`thumbnails`, { recursive: true });
+    await promises.mkdir(path.join(cwd, '../../', 'thumbnails'), {
+      recursive: true
+    });
   };
   // Check if directory exists in the parent directory.
-  access(path.join(cwd, '..', 'thumbnails'), constants.F_OK, (err) => {
+  access(path.join(cwd, '../..', 'thumbnails'), constants.F_OK, (err) => {
     console.log(
-      `${path.join(cwd, '..', 'thumbnails')} directory ${
+      `${path.join(cwd, '../..', 'thumbnails')} directory ${
         err ? 'does not exist' : 'exists'
       }`
     );
@@ -44,41 +46,57 @@ app.get('/convert', sharpLogger, async (req, res, next) => {
   const height = Number(req.query.height);
   const inputFile = fileName + '.jpg';
   const outputFile = `${fileName}_${width}_${height}.jpg`;
+
   // Check if the requested file and dimensions exist.
   try {
-    accessSync(path.join(cwd, '..', 'thumbnails', outputFile), constants.R_OK);
+    accessSync(
+      path.join(cwd, '../..', 'thumbnails', outputFile),
+      constants.R_OK
+    );
     console.log('requested file and dimensions exist');
     res
       .status(200)
-      .sendFile(path.join(cwd, '..', 'thumbnails', outputFile), function (err) {
-        if (err) {
-          res
-            .status(404)
-            .send(
-              'Bad Request: Please check the URL/filename and try again...'
+      .sendFile(
+        path.join(cwd, '../..', 'thumbnails', outputFile),
+        function (err) {
+          if (err) {
+            res
+              .status(404)
+              .send(
+                'Bad Request: Please check the URL/filename and try again...'
+              );
+            next(err);
+          } else {
+            console.log(
+              'Sent:',
+              path.join(cwd, '../..', 'thumbnails', outputFile)
             );
-          next(err);
-        } else {
-          console.log('Sent:', path.join(cwd, '..', 'thumbnails', outputFile));
+          }
         }
-      });
+      );
   } catch (err) {
     console.error('requested file and dimensions processing...');
     await resize(inputFile, outputFile, width, height);
     res
       .status(200)
-      .sendFile(path.join(cwd, '..', 'thumbnails', outputFile), function (err) {
-        if (err) {
-          res
-            .status(404)
-            .send(
-              'Bad Request: Please check the URL/filename and try again...'
+      .sendFile(
+        path.join(cwd, '../..', 'thumbnails', outputFile),
+        function (err) {
+          if (err) {
+            res
+              .status(404)
+              .send(
+                'Bad Request: Please check the URL/filename and try again...'
+              );
+            next(err);
+          } else {
+            console.log(
+              'Sent:',
+              path.join(cwd, '../..', 'thumbnails', outputFile)
             );
-          next(err);
-        } else {
-          console.log('Sent:', path.join(cwd, '..', 'thumbnails', outputFile));
+          }
         }
-      });
+      );
   }
 });
 // start the Express server
@@ -86,4 +104,4 @@ app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
 
-export default app;
+export { app, resize };
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,18 +1,24 @@
 import supertest from 'supertest';
-import app from '../index';
+import { app, resize } from '../index';
 
 const request = supertest(app);
+const resize_ = supertest(resize);
+
 describe('Test endpoint responses', () => {
-  it('gets the api endpoint 1', async () => {
-    const response = await request.get('/convert').query({
+  it('gets the image conversion endpoint with correct query', async () => {
+    const response = await request.get('/convert/').query({
       filename: 'fjord',
       width: '200',
       height: '200'
-    }); //?filename=fjord&width=200&height=200');
+    });
     expect(response.status).toBe(200);
   });
-  /*it('gets the api endpoint 2', async () => {
-    const response = await request.get('/convert/?filename=ford&width=200&height=200');
+  it('gets the image conversion endpoint with incorrect query', async () => {
+    const response = await request.get('/convert/').query({
+      filename: 'ford',
+      width: '300',
+      height: '300'
+    });
     expect(response.status).toBe(404);
-  });*/
+  });
 });
